528a0cdad2a2cb73375ca877c0060ba1b7dabc44
fisher <fishercomes@163.com>
2021-02-21 10:25:12 +0800

    设计模式-建造者模式

---
+++ design_pattern/builder/src/main/java/com/fisher/builder/Armor.java
@@ -0,0 +1,10 @@
+package com.fisher.builder;
+
+/**
+ * @Description
+ * @Author fisher
+ * @Date 2021-02-20 10:10
+ */
+
+public class Armor {
+}
---
+++ design_pattern/builder/src/main/java/com/fisher/builder/Weapon.java
@@ -0,0 +1,10 @@
+package com.fisher.builder;
+
+/**
+ * @Description
+ * @Author fisher
+ * @Date 2021-02-20 10:11
+ */
+
+public class Weapon {
+}
