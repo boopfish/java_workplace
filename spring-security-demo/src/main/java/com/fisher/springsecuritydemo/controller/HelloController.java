54c190c5f3b1fb7af6836f26ff9f8e709b1e488e
fisher <fishercomes@163.com>
2021-02-22 23:43:39 +0800

    spring security 初始化

---
+++ spring-security-demo/src/main/java/com/fisher/springsecuritydemo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.fisher.springsecuritydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * https://www.cnblogs.com/lenve/p/11242055.html
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
