2cdc54cb2b1588a320e90f2b48c7414bf68aa5bc
fisher <fishercomes@163.com>
2021-03-02 16:32:41 +0800

    11

--- spring-demo/src/main/java/com/fisher/App.java
+++ spring-demo/src/main/java/com/fisher/App.java
@@ -1,18 +1,35 @@
 package com.fisher;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fisher.service.MessageService;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import okhttp3.*;
+import okhttp3.Request.Builder;
+
+import java.io.IOException;
+
 
 /**
  * Hello world!
  */
 public class App {
-	public static void main(String[] args) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
-		System.out.println("Spring容器启动成功");
-		System.out.println(applicationContext.getBean(MessageService.class).getMessage());
+	public static void main(String[] args) throws IOException {
+		OkHttpClient okHttpClient = new OkHttpClient();
+		System.out.println(MediaType.get("application/json;charset=utf-8"));
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("autoDoorIp", "test");
+		jsonObject.put("autoDoorSn", 123);
+		jsonObject.put("classroomId", "test");
+		jsonObject.put("controlPanelIp", "test");
+		jsonObject.put("mac", "test");
+
+		RequestBody body = RequestBody.create(MediaType.parse("application/json;charset=utf-8"), jsonObject.toJSONString());
+
+		Request request = new Request.Builder()
+				.url("http://192.168.0.39:7005/cp/toServer/autoRegister")
+				.post(body)
+				.build();
+		Call call = okHttpClient.newCall(request);
+		Response re = call.execute();
 
 	}
 }
